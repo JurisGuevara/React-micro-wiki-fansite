792cdeccd9289b1487204968b9b23c2ebbe55d07
Juris Guevara <tertullanojuris@gmail.com>
2022-11-06 13:16:47 +0800

    People tabs: - added more image URLs - completed development of People component

--- src/components/tabs/People.js
+++ src/components/tabs/People.js
@@ -51,81 +51,73 @@ const People = () => {
   ]
 
   const imagesPage4 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/736x/db/a6/a1/dba6a196220f2f6a4cbc17cfd4aec6ac.jpg',
+    'https://i.pinimg.com/originals/13/bb/7d/13bb7d3b9533f99e71cc357b5cef6e40.jpg',
+    'https://i.pinimg.com/474x/29/16/62/29166227119bf6376119c9d89c3d88c7--terence-stamp-star-wars-disney.jpg',
+    'https://i.pinimg.com/736x/54/f5/45/54f545886b41311a07c6ab046e8344d0.jpg',
+    'https://i.pinimg.com/originals/49/0f/c2/490fc2527acc54fa5ecb193cc25b6981.jpg',
+    'https://i.pinimg.com/originals/26/eb/28/26eb282e9f786585e8eba1c82192baed.jpg',
+    'https://i.pinimg.com/originals/f2/ed/a2/f2eda28291597c0fd06db49d6628059e.jpg',
+    'https://i.pinimg.com/originals/2d/70/8f/2d708f343510011f554d2da0d84c2f72.jpg',
+    'https://i.pinimg.com/originals/41/0f/51/410f51eb9297df6f6e9defc501afeda1.jpg',
+    'https://i.pinimg.com/474x/fa/8a/2d/fa8a2d8bd8b71164c5d4214820bc2909--police-image.jpg'
   ]
 
   const imagesPage5 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/736x/b3/bf/4b/b3bf4b0d16aa6941a39ebe04bf973f7b--queen-amidala-padm%C3%A9-amidala.jpg',
+    'https://i.pinimg.com/736x/74/ef/62/74ef6255585260ddfcc1488bd6d0d8d7.jpg',
+    'https://i.pinimg.com/originals/47/7d/51/477d51544188f32402e73109b3510ad6.jpg',
+    'https://cdn.shopify.com/s/files/1/0279/0739/1585/products/F3417_PROD_SW_BL_PROVIDENCE_0001_Online_2000SQ_grande.jpg?v=1639757226',
+    'https://i.pinimg.com/originals/9b/45/b4/9b45b4091b7123d189cfe3bf53898ecf.jpg',
+    'https://rpggamer.org/uploaded_images/RattsHS.jpg',
+    'https://i.pinimg.com/originals/04/e7/a2/04e7a27ecec22fe1c0fffed7a22bb875.jpg',
+    'https://i.pinimg.com/564x/f4/d4/e3/f4d4e3eb5e888aa1108ee2e112cdffa4.jpg',
+    'https://i.pinimg.com/474x/10/d9/aa/10d9aa093da2a4d52b9a92c71cdf1e5a.jpg',
+    'https://i.pinimg.com/originals/79/20/32/79203274bca96ed27c52530e855ccdb8.jpg'
   ]
 
   const imagesPage6 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/736x/c6/51/0c/c6510c60b7244d557966ba2e45a9d767.jpg',
+    'https://i.pinimg.com/originals/e9/74/5d/e9745dbfb3d95d1094138a2dc07cd0de.jpg',
+    'https://i.pinimg.com/originals/26/74/e4/2674e46463cf739af41439c426ad0d7c.jpg',
+    'https://db4sgowjqfwig.cloudfront.net/images/5605108/Adi_Gallia.png',
+    'https://i.pinimg.com/originals/e6/37/d3/e637d3e2bb0400ceac248cc8a5bdfc39.jpg',
+    'https://i.pinimg.com/originals/41/55/1b/41551bad737ac712683caad4c828a940.jpg',
+    'https://i.pinimg.com/736x/56/fd/eb/56fdeb4b59d549dc3bd3df0cb3bc01f2.jpg',
+    'https://i.pinimg.com/originals/83/b0/d0/83b0d0ffdeb5ebf916954672f6751da6.jpg',
+    'https://i.pinimg.com/474x/56/4c/75/564c757171c56d012ccf277e13fc84e1.jpg',
+    'https://pbs.twimg.com/media/FTcntHGXwAEDwN0.jpg'
   ]
 
   const imagesPage7 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/550x/32/8b/92/328b92044486e11deddfb85fa7f52bf2.jpg',
+    'https://i.pinimg.com/474x/55/20/62/552062e77eb74a3f3f7f2c1cb4fad751--war-machine-clone-wars.jpg',
+    'https://i.pinimg.com/originals/bf/04/e3/bf04e3a00b07c248ea18d6a0e0583cf0.jpg',
+    'https://i.pinimg.com/474x/4f/c7/6f/4fc76fea19eaeac7e5cbf700fb820dd3.jpg',
+    'https://i.pinimg.com/originals/b3/ab/fd/b3abfd216b1c344116f996cc7e3b3fa4.jpg',
+    'https://i.pinimg.com/originals/2f/8b/f6/2f8bf6bb744e5d1f0b1c87152722c2e2.jpg',
+    'https://i.pinimg.com/736x/66/8e/ce/668ece69a71fa3358fe38d8836860278.jpg',
+    'https://i.pinimg.com/736x/d8/57/b7/d857b7460ec7140849548b4443e5e85e.jpg',
+    'https://i.pinimg.com/originals/bb/d7/44/bbd744db73d429e05c60d87643d1b6fe.jpg',
+    'https://i.pinimg.com/736x/5b/81/20/5b8120170c7682e13a63194d9858cb95--dexter-star-wars-species.jpg'
   ]
 
   const imagesPage8 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/originals/24/6e/78/246e78e3d76d7c5bc153860a944217bf.jpg',
+    'https://i.pinimg.com/originals/5f/13/25/5f13253baf2420ad1023d1774fbecb4d.jpg',
+    'https://i.pinimg.com/originals/93/aa/1b/93aa1bc976083b04ae5ee932082498a3.jpg',
+    'https://i.pinimg.com/originals/e7/f3/db/e7f3dbe295e37e3c87bf88b3002bc639.jpg',
+    'https://i.pinimg.com/originals/01/d0/c6/01d0c6b1ebc30ecd8f448803a00fc2c0.jpg',
+    'https://i.pinimg.com/originals/b1/6c/4f/b16c4f77674ec3fd1c702f3e62811c2e.jpg',
+    'https://i.pinimg.com/originals/79/c4/e4/79c4e4ed78809790b55dcdd9aa1d82f1.jpg',
+    'https://i.pinimg.com/originals/40/d4/70/40d470aff09ec66518ea482c6bc569a5.jpg',
+    'https://i.pinimg.com/originals/27/5a/13/275a131bad944c9a86851bb9236d016e.jpg',
+    'https://i.pinimg.com/originals/ea/d1/25/ead1250106b60318cf6276934e6b70a7.jpg'
   ]
 
   const imagesPage9 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'https://i.pinimg.com/originals/6c/af/e9/6cafe9ede6a49b4f81c68585444824ad.jpg',
+    'https://i.pinimg.com/474x/53/9e/65/539e652facea73504bfc1fd2812a8d94.jpg'
   ]
 
   const setImagePage = () => {
